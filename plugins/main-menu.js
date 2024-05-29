50a15e67aa309793bb4db32a7a1d6d95805280ad
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-29 20:29:40 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -154,7 +154,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 //handler.help = ['help']
 //handler.tags = ['main']
 handler.command = ['اوامر', 'أوامر', 'المهام'] 
-handler.register = true
+handler.register = false
 
 export default handler
 
