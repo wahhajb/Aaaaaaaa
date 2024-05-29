60ee6a05cafbfc7d51c686f37cae5260d82dee5a
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-29 15:30:11 +0300

    Create تطقيم2.js

---
+++ plugins/تطقيم2.js
@@ -0,0 +1,17 @@
+import fetch from "node-fetch"
+let handler = async (m, { conn }) => {
+
+  let data = await (await fetch('https://raw.githubusercontent.com/Afghhjjkoo/GURU-BOT/main/lib/%D8%B7%D9%82%D9%85%20%D8%AD%D8%A8.json')).json()
+  let cita = data[Math.floor(Math.random() * data.length)]
+  
+  let cowi = await(await fetch(cita.cowo)).buffer()
+  await conn.sendFile(m.chat, cowi, '', '*بنت*👧', m)
+  let ciwi = await(await fetch(cita.cewe)).buffer()
+  await conn.sendFile(m.chat, ciwi, '', '*ولد*🧑', m)
+}
+handler.help = ['تطقيم2']
+handler.tags = ['fun']
+handler.command = /^2طقم2|تطقيم$/i
+handler.limit = true
+
+export default handler
