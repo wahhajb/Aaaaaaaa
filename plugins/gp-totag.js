75ed22454502d137d150fc5a49e3ce74f91d0911
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-30 16:23:28 +0300

    Update gp-totag.js

--- plugins/gp-totag.js
+++ plugins/gp-totag.js
@@ -6,9 +6,9 @@ let handler = async (m, { conn, text, participants}) => {
     conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
 }
 
-handler.help = ['totag']
+handler.help = ['منشن']
 handler.tags = ['group']
-handler.command = /^(totag|tag)$/i
+handler.command = /^(منشن|tag)$/i
 
 handler.admin = true
 handler.group = true
