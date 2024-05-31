6e25ab82bea47b21a433112b940e0b7ca35d4934
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-31 19:46:41 +0300

    Update dl-playlist.js

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
         });
     }
 
-    await conn.sendList(m.chat, '  ≡ *𝑴𝒊𝒓𝒛𝒂 𝑴𝒖𝒔𝒊𝒄*🔎', `\n 📀 النتيجة:\n *${text}*`, `اضغط هنا`, ytres[0].image, listSections, m);
+    await conn.sendList(m.chat, '*•┈┈••●◆♪ 𝑴𝒊𝒓𝒛𝒂 𝑴𝒖𝒔𝒊𝒄 ♪◆●••┈┈•*', `\n 📀 النتيجة:\n *${text}*`, `اضغط هنا`, ytres[0].image, listSections, m);
 };
 
 handler.help = ['شغل2']
