fd34404454e5f5189771b44c1a110f818755e34c
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-31 21:21:13 +0300

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -6,9 +6,9 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     m.reply(`✅ ${msg.leaMsgOn}`)
   } else throw `✳️ ${mssg.leaMsg}`
 }
-handler.help = ['setbye <text>']
+handler.help = ['تعديل المغادرة']
 handler.tags = ['group']
-handler.command = ['setbye'] 
+handler.command = ['تعديل المغادرة'] 
 handler.admin = true
 handler.owner = false
 
