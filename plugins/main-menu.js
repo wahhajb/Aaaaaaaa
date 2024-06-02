8f467c04299eff39a821c7498f361923f24bf1f7
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-06-02 13:51:31 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -158,7 +158,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
                         serverMessageId: -1
                     }
                 }
-            },{ quoted: global.fcontact } , m, null)
+            }, m, null)
   
     m.react('📑') 
     
