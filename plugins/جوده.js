6ba4f370ae4024f8d6f4cae400583895d1ccb62d
ڪَباؘسِ丫:١⁶𝑘𝑎𝑝𝑎𝑠 <167807989+TOXIC-KDIS@users.noreply.github.com>
2024-06-05 21:16:34 +0300

    Create جوده.js

---
+++ plugins/جوده.js
@@ -0,0 +1,52 @@
+import FormData from "form-data";
+import Jimp from "jimp";
+
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/es.json`))
+  const tradutor = _translate.plugins.herramientas_hd
+
+ try {    
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || q.mediaType || "";
+  if (!mime) throw `*❆━━━═⏣⊰🦇⊱⏣═━━━❆*\n\n*الرجاء الرد على صورة*\n\n*❆━━━═⏣⊰🦇⊱⏣═━━━❆*`;
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `الصيغة المدعومة فقط هي (jpeg و png) (${mime}) غير مدعومة`;
+  m.reply("*❆━━━═⏣⊰🦇⊱⏣═━━━❆*\n\n*جاري تحسين الصورة...*\n\n*❆━━━═⏣⊰🦇⊱⏣═━━━❆*");
+  let img = await q.download?.();
+  let pr = await remini(img, "enhance");
+  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
+ } catch {
+  throw "*❆━━━═⏣⊰🦇⊱⏣═━━━❆*\n\n*الرجاء الرد على صورة*\n\n*❆━━━═⏣⊰🦇⊱⏣═━━━❆*";
+ }
+};
+handler.help = ["remini", "hd", "enhance"];
+handler.tags = ["ai", "tools"];
+handler.command = ["remini", "جوده", "جودة"];
+export default handler;
+
+async function remini(imageData, operation) {
+  return new Promise(async (resolve, reject) => {
+    const availableOperations = ["enhance", "recolor", "dehaze"];
+    if (availableOperations.includes(operation)) {
+      operation = operation;
+    } else {
+      operation = availableOperations[0];
+    }
+    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
+    const formData = new FormData();
+    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
+    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
+    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
+      function (err, res) {
+        if (err) reject(err);
+        const chunks = [];
+        res.on("data", function (chunk) {chunks.push(chunk)});
+        res.on("end", function () {resolve(Buffer.concat(chunks))});
+        res.on("error", function (err) {
+        reject(err);
+        });
+      },
+    );
+  });
+}
