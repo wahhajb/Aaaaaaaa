67a23ffb392d7f300f487b8fd41ca254651a6591
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-06-13 22:27:41 +0300

    Create موت.js

---
+++ plugins/موت.js
@@ -0,0 +1,23 @@
+//مقدمه من قناه: https://whatsapp.com/channel/0029VaZQ0FG4dTnQxihmuk01
+//المقدم:زيريف دراجونيل
+let handler = async (m, { conn }) => {
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let name = conn.getName(who)
+    let pp = await conn.profilePictureUrl(who,  image ).catch(_ =>  ./mob.jpg )
+    conn.sendFile(m.chat, global.API( https://some-random-api.com ,  /canvas/overlay/wasted , {
+      avatar: pp, 
+    }),  waste.png , `*⎔❯╼╾╾╃❖⟪ 🍥 ⟫❖╃╼╾╾❮⎔*
+*⌝🪦┇لقد مات┇🪦⌞*
+*⎔❯╼╾╾╃❖⟪ 🍥 ⟫❖╃╼╾╾❮⎔*
+*♡↵》كان راجل جدع*
+*♡↵》المرحوم『 ${name} 』•*
+*‏◉ ⊱━─━─━ •♦️• ━─━─━⊰ ◉*
+『- 𝑁𝐴𝑇𝑺𝑈 𝗕𝗢𝗧』
+*⎔❯╼╾╾╃❖⟪ 🍥 ⟫❖╃╼╾╾❮⎔*, m)
+  }
+  
+  handler.help = [ waste @user ]
+  handler.tags = [ fun ]
+  handler.command = [ موت ] 
+
+  export default handler
